8427bd1059d071ef30bdd6f349684ecbb66d4418
Togzhan Atibek <togzhan@MacBook-Togzhan.local>
2023-04-21 15:27:04 +0600

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class test {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void test() {
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page: " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+}
